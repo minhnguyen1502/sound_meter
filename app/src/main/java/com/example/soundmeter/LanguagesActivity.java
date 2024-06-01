6a795da1295c6650c4ec449e41f52b3b0bd57db1
minhnguyen1502 <minhnguyen010502@gmail.com>
2024-06-01 15:16:38 +0700

    update in laptop

--- app/src/main/java/com/example/soundmeter/LanguagesActivity.java
+++ app/src/main/java/com/example/soundmeter/LanguagesActivity.java
@@ -14,6 +14,8 @@ import java.util.Locale;
 public class LanguagesActivity extends AppCompatActivity {
 
     LanguagesBinding languagesBinding;
+    private String selectedLanguage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,56 +26,57 @@ public class LanguagesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                changeLanguage("en");
-                Toast.makeText(LanguagesActivity.this, "You changed language to English", Toast.LENGTH_SHORT).show();
+                selectedLanguage = "en";
+                Toast.makeText(LanguagesActivity.this, "You choose English", Toast.LENGTH_SHORT).show();
             }
         });
         languagesBinding.constraintFrench.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("fr");
-
-                Toast.makeText(LanguagesActivity.this, "You changed language to French", Toast.LENGTH_SHORT).show();
+                selectedLanguage = "fr";
+                Toast.makeText(LanguagesActivity.this, "You choose French", Toast.LENGTH_SHORT).show();
             }
         });
         languagesBinding.constraintGerman.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("de");
 
-                Toast.makeText(LanguagesActivity.this, "You changed language to German", Toast.LENGTH_SHORT).show();
+                selectedLanguage = "de";
+
+                Toast.makeText(LanguagesActivity.this, "You choose German", Toast.LENGTH_SHORT).show();
             }
         });
         languagesBinding.constraintHindi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("hi");
 
-                Toast.makeText(LanguagesActivity.this, "You changed language to Hindi", Toast.LENGTH_SHORT).show();
+                changeLanguage("hi");
+                Toast.makeText(LanguagesActivity.this, "You choose Hindi", Toast.LENGTH_SHORT).show();
 
             }
         });
         languagesBinding.constraintIndo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("in");
+                selectedLanguage = "in";
 
-                Toast.makeText(LanguagesActivity.this, "You changed language to Indonesia", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LanguagesActivity.this, "You choose Indonesia", Toast.LENGTH_SHORT).show();
             }
         });
         languagesBinding.constraintSpanish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("es");
-                Toast.makeText(LanguagesActivity.this, "You changed language to Spanish", Toast.LENGTH_SHORT).show();
+                selectedLanguage = "es";
+
+                Toast.makeText(LanguagesActivity.this, "You choose Spanish", Toast.LENGTH_SHORT).show();
 
             }
         });
         languagesBinding.constraintPortu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                changeLanguage("pt");
-
-                Toast.makeText(LanguagesActivity.this, "You changed language to Portuguese", Toast.LENGTH_SHORT).show();
+                selectedLanguage = "pt";
+                Toast.makeText(LanguagesActivity.this, "You choose Portuguese", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -86,27 +89,23 @@ public class LanguagesActivity extends AppCompatActivity {
         languagesBinding.ivConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(LanguagesActivity.this, "You changed language success", Toast.LENGTH_SHORT).show();
+                if (selectedLanguage == null) {
+                    Toast.makeText(LanguagesActivity.this, "choose language before change", Toast.LENGTH_SHORT).show();
+                } else {
+                    changeLanguage(selectedLanguage);
+                    Toast.makeText(LanguagesActivity.this, "You changed language success", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-    public void changeLanguage(String language){
+    public void changeLanguage(String language) {
         Locale locale = new Locale(language);
         Locale.setDefault(locale);
         Configuration configuration = new Configuration();
-        configuration.locale=locale;
+        configuration.locale = locale;
         getApplication().getResources().updateConfiguration(configuration,
                 getApplication().getResources().getDisplayMetrics());
-
-        languagesBinding.tvLanguage.setText(getString(R.string.language));
-        languagesBinding.tvEng.setText(getString(R.string.english));
-        languagesBinding.tvDe.setText(getString(R.string.german));
-        languagesBinding.tvEs.setText(getString(R.string.spanish));
-        languagesBinding.tvHi.setText(getString(R.string.hindi));
-        languagesBinding.tvPt.setText(getString(R.string.portuguese));
-        languagesBinding.tvFr.setText(getString(R.string.french));
-        languagesBinding.tvIn.setText(getString(R.string.indonesia));
 
 
     }
